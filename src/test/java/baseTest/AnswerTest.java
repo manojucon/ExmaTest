d1e672664d159ce4dc0a0676d6fa40a33adcb490
Manoj Kumar <manojucon@gmail.com>
2024-09-27 11:33:47 +0530

    Updated code for hindi character

--- src/test/java/baseTest/AnswerTest.java
+++ src/test/java/baseTest/AnswerTest.java
@@ -2,9 +2,8 @@ package baseTest;
 
 import org.testng.annotations.BeforeSuite;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 import java.util.Properties;
 
 public class AnswerTest extends BaseTest {
@@ -28,16 +27,8 @@ public class AnswerTest extends BaseTest {
 
 
 
-
     public void propertiesSetup() {
         try {
-            answer12 = new FileInputStream("src/test/java/baseTest/testAnswer12.properties");
-            answer11 = new FileInputStream("src/test/java/baseTest/testAnswer11.properties");
-            answer10 = new FileInputStream("src/test/java/baseTest/testAnswer10.properties");
-            answer9 = new FileInputStream("src/test/java/baseTest/testAnswer9.properties");
-            answer8 = new FileInputStream("src/test/java/baseTest/testAnswer8.properties");
-            answer7 = new FileInputStream("src/test/java/baseTest/testAnswer7.properties");
-            answer6 = new FileInputStream("src/test/java/baseTest/testAnswer6.properties");
             answerProperty12 = new Properties();
             answerProperty11 = new Properties();
             answerProperty10 = new Properties();
@@ -45,7 +36,26 @@ public class AnswerTest extends BaseTest {
             answerProperty8 = new Properties();
             answerProperty7 = new Properties();
             answerProperty6 = new Properties();
+            answerProperty9.load(new InputStreamReader(new FileInputStream("src/test/java/baseTest/testAnswer9.properties"),"UTF-8"));
+            answerProperty10.load(new InputStreamReader(new FileInputStream("src/test/java/baseTest/testAnswer10.properties"),"UTF-8"));
+            answerProperty11.load(new InputStreamReader(new FileInputStream("src/test/java/baseTest/testAnswer11.properties"),"UTF-8"));
+            answerProperty12.load(new InputStreamReader(new FileInputStream("src/test/java/baseTest/testAnswer12.properties"),"UTF-8"));
+            answerProperty6.load(new InputStreamReader(new FileInputStream("src/test/java/baseTest/testAnswer6.properties"),"UTF-8"));
+            answerProperty7.load(new InputStreamReader(new FileInputStream("src/test/java/baseTest/testAnswer7.properties"),"UTF-8"));
+            answerProperty8.load(new InputStreamReader(new FileInputStream("src/test/java/baseTest/testAnswer8.properties"),"UTF-8"));
+            answer12 = new FileInputStream("src/test/java/baseTest/testAnswer12.properties");
+            answer11 = new FileInputStream("src/test/java/baseTest/testAnswer11.properties");
+            answer10 = new FileInputStream("src/test/java/baseTest/testAnswer10.properties");
+            answer9 = new FileInputStream("src/test/java/baseTest/testAnswer9.properties");
+            answer8 = new FileInputStream("src/test/java/baseTest/testAnswer8.properties");
+            answer7 = new FileInputStream("src/test/java/baseTest/testAnswer7.properties");
+            answer6 = new FileInputStream("src/test/java/baseTest/testAnswer6.properties");
+
         } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
@@ -53,7 +63,7 @@ public class AnswerTest extends BaseTest {
             answerProperty12.load(answer12);
             answerProperty11.load(answer11);
             answerProperty10.load(answer10);
-            answerProperty9.load(answer9);
+          /*  answerProperty9.load(answer9);*/
             answerProperty8.load(answer8);
             answerProperty7.load(answer7);
             answerProperty6.load(answer6);
